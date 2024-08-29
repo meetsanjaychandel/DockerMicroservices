c6b0b568c213d298c92f9279236d73e6f4603e0d
meetsanjaychandel <meetsanjaychandel@gmail.com>
2024-08-30 01:06:38 +0530

    Revert "uploading final tested app"
    This reverts commit a85f523376b6e926ce82d5a2aca51d056bfed207.

--- receiverservice/src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const {receiveData} = require("../controllers/user.controllers.js");
-
-const Router = require('express');
-const router = Router();
-
-router.route("/adduser").post(receiveData);
-
-module.exports= router;
